a196f659306e575d4d8f3c6c3559eca11452e473
harijerryz <112164315+harijerryz@users.noreply.github.com>
2023-05-03 14:45:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,7 @@ terraform {
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
+    access_key = "AKIAUFKOXY2YTHWFEAVZ"
+    secret_key = "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"
   }
 }
